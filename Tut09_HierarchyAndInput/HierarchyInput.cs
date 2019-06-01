53d5612e3c65495f876a722bcb19b13ad5f93155
elisabethkuellmer <48796369+elisabethkuellmer@users.noreply.github.com>
2019-06-01 16:21:37 +0200

    Add files via upload

--- Tut09_HierarchyAndInput/HierarchyInput.cs
+++ Tut09_HierarchyAndInput/HierarchyInput.cs
@@ -16,10 +16,20 @@ namespace Fusee.Tutorial.Core
 {
     public class HierarchyInput : RenderCanvas
     {
+
         private SceneContainer _scene;
         private SceneRenderer _sceneRenderer;
         private float _camAngle = 0;
+        private float _camSpeed = 0;
         private TransformComponent _baseTransform;
+        private TransformComponent _bodyTransform;
+        private TransformComponent _upperArmTransform;
+        private TransformComponent _foreArmTransform;
+        private TransformComponent _RightHandTransform;
+        private TransformComponent _LeftHandTransform;
+
+
+
 
         SceneContainer CreateScene()
         {
@@ -29,6 +39,39 @@ namespace Fusee.Tutorial.Core
                 Rotation = new float3(0, 0, 0),
                 Scale = new float3(1, 1, 1),
                 Translation = new float3(0, 0, 0)
+            };
+
+            _bodyTransform = new TransformComponent
+            {
+                Rotation = new float3(0,0,0),
+                Scale = new float3(1,1,1),
+                Translation = new float3(0,6,0)
+            };
+
+            _upperArmTransform = new TransformComponent
+            {
+                Rotation = new float3(0,0,0),
+                Scale = new float3(1,1,1),
+                Translation = new float3(2,4,0)
+            };
+
+            _foreArmTransform = new TransformComponent
+            {
+                Rotation = new float3(0,0,0),
+                Scale = new float3(1,1,1),
+                Translation = new float3(-2,4,0)
+            };
+            _RightHandTransform = new TransformComponent
+            {
+                Rotation = new float3(0,0,0),
+                Scale = new float3(1,1,1),
+                Translation = new float3(0.5f,4.5f,0)
+            };
+            _LeftHandTransform = new TransformComponent
+            {
+                Rotation = new float3(0,0,0),
+                Scale = new float3(1,1,1),
+                Translation = new float3(-0.5f,4.5f,0)
             };
 
             // Setup the scene graph
@@ -41,21 +84,162 @@ namespace Fusee.Tutorial.Core
                         Components = new List<SceneComponentContainer>
                         {
                             // TRANSFROM COMPONENT
-                            _baseTransform,
+                             _baseTransform,
 
                             // SHADER EFFECT COMPONENT
                             new ShaderEffectComponent
                             {
                                 Effect = SimpleMeshes.MakeShaderEffect(new float3(0.7f, 0.7f, 0.7f), new float3(0.7f, 0.7f, 0.7f), 5)
+                               //Diffuse = new MatChannelContainer {Color = new float3(0.7f,0.7f,0.7f)},
+                               //Specular = new SpecularChannelContainer {Color = new float3(0,0,0), Shininess = 5}
                             },
 
                             // MESH COMPONENT
                             SimpleMeshes.CreateCuboid(new float3(10, 2, 10))
                         }
-                    }
-                }
-            };
-        }
+                    },
+                    //Red Body
+                    new SceneNodeContainer 
+                    {
+                        Components = new List<SceneComponentContainer>
+                        {
+                            _bodyTransform,
+                            new ShaderEffectComponent
+                            {
+                                Effect = SimpleMeshes.MakeShaderEffect(new float3(1f, 0f, 0f), new float3(0.7f, 0.7f, 0.7f), 5)
+                               // Diffuse = new MatChannelContainer {Color = new float3(1,0,0)},
+                               // Specular = new SpecularChannelContainer {Color = new float3(1,1,1), Shininess = 5}
+                            },
+                            SimpleMeshes.CreateCuboid(new float3(2,10,2))
+                        },
+                        Children = new List<SceneNodeContainer>
+                        {
+                            //Green Upperarm
+                            new SceneNodeContainer
+                            {
+                                Components = new List<SceneComponentContainer>
+                                {
+                                _upperArmTransform 
+                                },
+                                Children = new List<SceneNodeContainer>
+                                {
+                                    new SceneNodeContainer
+                                    {
+                                        Components = new List<SceneComponentContainer>
+                                        {
+                                            new TransformComponent
+                                            {
+                                                Rotation = new float3(0,0,0),
+                                                Scale = new float3(1,1,1),
+                                                Translation = new float3(0,4,0)
+                                            },
+                                            new ShaderEffectComponent
+                                            {
+                                                Effect = SimpleMeshes.MakeShaderEffect(new float3(0,1,0), new float3(0.7f,0.7f,0.7f),5)
+                                            },
+                                            SimpleMeshes.CreateCuboid(new float3(2,10,2))
+                                        },
+                                    Children = new List<SceneNodeContainer>
+                                        {
+                                        //Blue Forearm
+                                        new SceneNodeContainer
+                                            {
+                                            Components = new List<SceneComponentContainer>
+                                                {
+                                                    _foreArmTransform
+                                                },
+                                            Children = new List<SceneNodeContainer>
+                                                {
+                                                    new SceneNodeContainer
+                                                    {
+                                                        Components = new List<SceneComponentContainer>
+                                                        {
+                                                            new TransformComponent
+                                                            {
+                                                                Rotation = new float3(0,0,0),
+                                                                Scale = new float3(1,1,1),
+                                                                Translation = new float3(0,4,0)
+                                                            },
+                                                            new ShaderEffectComponent
+                                                            {
+                                                                Effect = SimpleMeshes.MakeShaderEffect(new float3(0,0,1), new float3(0.7f,0.7f,0.7f),5)
+                                                            },
+                                                            SimpleMeshes.CreateCuboid(new float3(2,10,2))   
+                                                         },
+                                                    Children = new List<SceneNodeContainer>
+                                                    {
+                                                        //RightHand
+                                                        new SceneNodeContainer
+                                                            {
+                                                                Components = new List<SceneComponentContainer>
+                                                                {
+                                                                     _RightHandTransform
+                                                                },
+                                                                 
+                                                                Children = new List<SceneNodeContainer>
+                                                                {
+                                                                     new SceneNodeContainer
+                                                                        {
+                                                                            Components = new List<SceneComponentContainer>
+                                                                            {
+                                                                                new TransformComponent
+                                                                                {
+                                                                                    Rotation = new float3(0,0,0),
+                                                                                    Scale = new float3(1,1,1),
+                                                                                    Translation = new float3(0,2.5f,0)
+                                                                                },
+                                                        
+                                                                                new ShaderEffectComponent
+                                                                                {
+                                                                                    Effect = SimpleMeshes.MakeShaderEffect(new float3(1,1,0), new float3(0.7f,0.7f,0.7f),5)
+                                                                                },
+                                                                                 SimpleMeshes.CreateCuboid(new float3(1,5,2))
+                                                                             }
+                                                                         }
+                                                                    }
+                                                                },
+                                                                     //LeftHand
+                                                                        new SceneNodeContainer
+                                                                            {
+                                                                                Components = new List<SceneComponentContainer>
+                                                                                {
+                                                                                     _LeftHandTransform
+                                                                                 },
+                                                                                 Children = new List<SceneNodeContainer>
+                                                                                {
+                                                                                    new SceneNodeContainer
+                                                                                    {
+                                                                                        Components = new List<SceneComponentContainer>
+                                                                                        {
+                                                                                             new TransformComponent
+                                                                                            {
+                                                                                                Rotation = new float3(0,0,0),
+                                                                                                Scale = new float3(1,1,1),
+                                                                                                Translation = new float3(0,2.5f,0)
+                                                                                            },
+                                                                
+                                                                                            new ShaderEffectComponent
+                                                                                            {
+                                                                                                Effect = SimpleMeshes.MakeShaderEffect(new float3(1,1,0), new float3(0.7f,0.7f,0.7f),5)
+                                                                                            },
+                                                                                        SimpleMeshes.CreateCuboid(new float3(1,5,2))
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                             };
+                         }
 
         // Init is called on startup. 
         public override void Init()
@@ -72,17 +256,64 @@ namespace Fusee.Tutorial.Core
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            // Clear the backbuffer
+              // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // Setup the camera 
             RC.View = float4x4.CreateTranslation(0, -10, 50) * float4x4.CreateRotationY(_camAngle);
+
+            //Bodyrotation
+            float bodyrot = _bodyTransform.Rotation.y;
+            bodyrot += 0.1f*Keyboard.LeftRightAxis;
+            _bodyTransform.Rotation = new float3(0,bodyrot,0);
+
+            float upperarmchange =_upperArmTransform.Rotation.x;
+            upperarmchange += 0.1f*Keyboard.UpDownAxis;
+            _upperArmTransform.Rotation = new float3(upperarmchange,0,0);
+
+            float foreArmchange = _foreArmTransform.Rotation.x;
+            foreArmchange += 0.1f*Keyboard.WSAxis;
+            _foreArmTransform.Rotation = new float3(foreArmchange,0,0);
+
+           if( Mouse.LeftButton){
+                _camSpeed = Mouse.Velocity.x;
+            }
+            else {
+                _camSpeed = _camSpeed * 0.9f;
+            }
+
+            _camAngle = _camAngle + 0.005f * _camSpeed * Time.DeltaTime;
+            
+            float left_hand_rotation_z = _LeftHandTransform.Rotation.z;  
+            left_hand_rotation_z += 2f * Keyboard.ADAxis * Time.DeltaTime;
+            if( left_hand_rotation_z > 1.5f) {
+                left_hand_rotation_z = 1.5f;
+            }
+            if( left_hand_rotation_z < 0) {
+                left_hand_rotation_z = 0;
+            }
+            _LeftHandTransform.Rotation = new float3(0, 0, left_hand_rotation_z);
+            
+            float right_hand_rotation_z = _RightHandTransform.Rotation.z;  
+            right_hand_rotation_z -= 2f * Keyboard.ADAxis * Time.DeltaTime;
+            if( right_hand_rotation_z < -1.5f) {
+                right_hand_rotation_z = -1.5f;
+            }
+            if( right_hand_rotation_z > 0) {
+                right_hand_rotation_z = 0;
+            }
+            _RightHandTransform.Rotation = new float3(0, 0, right_hand_rotation_z);
+
+
+
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
 
             // Swap buffers: Show the contents of the backbuffer (containing the currently rendered farame) on the front buffer.
             Present();
+
+            
         }
 
 
