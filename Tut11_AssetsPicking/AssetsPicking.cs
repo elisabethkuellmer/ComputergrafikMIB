b7e4413a225310319ab705ca272d0a40d102db97
elisabethkuellmer <48796369+elisabethkuellmer@users.noreply.github.com>
2019-06-29 17:02:01 +0200

    Add files via upload

--- Tut11_AssetsPicking/AssetsPicking.cs
+++ Tut11_AssetsPicking/AssetsPicking.cs
@@ -18,7 +18,19 @@ namespace Fusee.Tutorial.Core
     {
         private SceneContainer _scene;
         private SceneRenderer _sceneRenderer;
+        private ScenePicker _scenePicker;
+        private PickResult _currentPick;
+        private float3 _oldColor;
+        private float _direction;
         private TransformComponent _baseTransform;
+        private TransformComponent _rad1Transform;
+        private TransformComponent _rad2Transform;
+        private TransformComponent _rad3Transform;
+        private TransformComponent _rad4Transform;
+        private TransformComponent _griffTransform;
+        private float _camAngle = 0;
+        private float _camSpeed = 0;
+        
 
         SceneContainer CreateScene()
         {
@@ -63,22 +75,117 @@ namespace Fusee.Tutorial.Core
             // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
             RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
 
-            _scene = CreateScene();
+            _scene = AssetStorage.Get<SceneContainer>("Car1.fus");
+
+   
 
             // Create a scene renderer holding the scene above
             _sceneRenderer = new SceneRenderer(_scene);
+            _scenePicker = new ScenePicker(_scene);
+
         }
 
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            _baseTransform.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
+           // _baseTransform.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
+
+            _rad1Transform = _scene.Children.FindNodes(node => node.Name == "Rad1")?.FirstOrDefault()?.GetTransform();
+            float rad1Rotation = _rad1Transform.Rotation.y;
+            rad1Rotation += 2* Keyboard.UpDownAxis * DeltaTime;
+            _rad1Transform.Rotation = new float3( 0,rad1Rotation, 0);
+
+            _rad2Transform = _scene.Children.FindNodes(node => node.Name == "Rad2")?.FirstOrDefault()?.GetTransform();
+             float rad2Rotation = _rad2Transform.Rotation.y;
+            rad2Rotation += 2* Keyboard.UpDownAxis * DeltaTime;
+            _rad2Transform.Rotation = new float3( 0,rad2Rotation, 0);
+
+            _rad3Transform = _scene.Children.FindNodes(node => node.Name == "Rad3")?.FirstOrDefault()?.GetTransform();
+             float rad3Rotation = _rad3Transform.Rotation.y;
+            rad3Rotation += 2* Keyboard.UpDownAxis * DeltaTime;
+            _rad3Transform.Rotation = new float3( 0,rad3Rotation, 0);
+
+            _rad4Transform = _scene.Children.FindNodes(node => node.Name == "Rad4")?.FirstOrDefault()?.GetTransform();
+             float rad4Rotation = _rad4Transform.Rotation.y;
+            rad4Rotation += 2* Keyboard.UpDownAxis * DeltaTime;
+            _rad4Transform.Rotation = new float3( 0,rad4Rotation, 0);
+
+            _griffTransform = _scene.Children.FindNodes(node => node.Name == "Griff")?.FirstOrDefault()?.GetTransform();
+             float griffRotation = _griffTransform.Rotation.y;
+            griffRotation += 0.1f* Keyboard.UpDownAxis;
+        
+            if( griffRotation > 1.5f) {
+                griffRotation = 1.5f;
+            }
+            if( griffRotation < 0) {
+                griffRotation = 0;
+            }
+ 
+            _griffTransform.Rotation = new float3(0, griffRotation,0);
+
+            // Setup the camera 
+            RC.View = float4x4.CreateTranslation(0, -1, 10) * float4x4.CreateRotationX(-(float) Atan(15.0 / 40.0)) * float4x4.CreateRotationY(_camAngle);
+
+            if( Mouse.RightButton){
+                _camSpeed = Mouse.Velocity.x;
+            }
+            else {
+                _camSpeed = _camSpeed * 0.9f;
+            }
+
+            _camAngle = _camAngle + 0.005f * _camSpeed * Time.DeltaTime;
+
+            
+            if (Mouse.LeftButton)
+            {
+                float2 pickPosClip = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1, 1);
+                _scenePicker.View = RC.View;
+                _scenePicker.Projection = RC.Projection;
+                List<PickResult> pickResults = _scenePicker.Pick(pickPosClip).ToList();
+                
+                PickResult newPick = null;
+                if (pickResults.Count > 0)
+                {
+                    pickResults.Sort((a, b) => Sign(a.ClipPos.z - b.ClipPos.z));
+                    newPick = pickResults[0];
+                }
+                if (newPick?.Node != _currentPick?.Node)
+                {
+                    if (_currentPick != null)
+                    {
+                        _currentPick.Node.GetComponent<ShaderEffectComponent>().Effect.SetEffectParam("DiffuseColor",_oldColor);
+                    }
+                    if (newPick != null)
+                    { 
+                        _oldColor = (float3) newPick.Node.GetComponent<ShaderEffectComponent>().Effect.GetEffectParam("DiffuseColor");
+                        newPick.Node.GetComponent<ShaderEffectComponent>().Effect.SetEffectParam("DiffuseColor", new float3(_oldColor.r*0.5f,_oldColor.g*0.5f,_oldColor.b*0.5f));
+
+                    }
+                    _currentPick = newPick;
+                }
+                
+
+            }
 
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // Setup the camera 
-            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Atan(15.0 / 40.0));
+            //RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Atan(15.0 / 40.0));
+
+            if (Mouse.LeftButton)
+             {
+                float2 pickPosClip = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1, 1);
+                _scenePicker.View = RC.View;
+                _scenePicker.Projection = RC.Projection;
+                List<PickResult> pickResults = _scenePicker.Pick(pickPosClip).ToList();
+
+                if (pickResults.Count > 0)
+                {
+                    pickResults.Sort((a, b) => Sign(a.ClipPos.z - b.ClipPos.z));
+                    Diagnostics.Log(pickResults[0].Node.Name);
+                }
+            }
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
